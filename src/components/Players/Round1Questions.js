eabae793330d4206e46f15378dd347bb73bb333c
tmat06 <timmy.a.matthews@gmail.com>
2018-09-26 15:49:02 -0600

    timer working and not overlapping, closure method implemented, now just finish question sequence and show results page with additional styling

--- src/components/Players/Round1Questions.js
+++ src/components/Players/Round1Questions.js
@@ -1,18 +1,50 @@
 import React from "react";
 import { connect } from "react-redux";
+import io from "socket.io-client";
+
+const socket = io.connect(
+  "http://localhost:3006/",
+  {
+    reconnection: true,
+    reconnectionDelay: 1000,
+    reconnectionDelayMax: 5000,
+    reconnectionAttempts: 99999
+  }
+);
 
 class Round1Questions extends React.Component {
   constructor() {
     super();
+    this.state = {
+      tracker: 0,
+      timer: 5
+    };
+    socket.on("tracker-update", tracker => {
+      tracker === -1
+        ? this.props.history.push("/PlayerResults")
+        : this.setState({ tracker: tracker });
+    });
+    socket.on("timer-countdown", time => {
+      this.setState({ timer: time });
+    });
+  }
+
+  componentDidMount() {
+    socket.emit("join-room", { room: this.props.room });
   }
 
   render() {
-    return <div>Round1Questions</div>;
+    return (
+      <div>
+        {this.state.timer}
+        {this.props.questions[this.state.tracker].question}
+      </div>
+    );
   }
 }
 
 function mapStateToProps(state) {
-  return { questions: state.questions };
+  return { questions: state.questions, room: state.room };
 }
 
 export default connect(mapStateToProps)(Round1Questions);

bd145b9e02c0c604b197a9b659e731c42e2ede5f
Tim Matthews <timmy.a.matthews@gmail.com>
2018-10-16 11:46:23 -0600

    added express.static

--- server/server.js
+++ server/server.js
@@ -11,6 +11,8 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use(express.static(`${__dirname}/../build`));
+
 massive(CONNECTION_PATH).then(massiveInstance => {
   app.set("db", massiveInstance);
 });

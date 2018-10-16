2f5bac1337781c0ef82e4283cbd77e6928f9c33d
Tim Matthews <timmy.a.matthews@gmail.com>
2018-10-16 14:34:36 -0600

    working

--- server/server.js
+++ server/server.js
@@ -36,10 +36,10 @@ app.get("/room-check/:room", (req, res) => {
     console.log("response in server", response);
     if (response[0]) {
       //indicating that room is joinable
-      res.status(200).send("true");
+      res.status(200).send("roomFound");
     } else {
       //indicating that room has not been made yet
-      res.sendStatus(200);
+      res.status(200).send("noRoom");
     }
   });
 });
